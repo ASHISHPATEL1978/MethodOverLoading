d4388006960df442944ae9d8eb86c78da175ffe4
ASHISH PATEL <ASHISHPATEL@GMAIL.com>
2023-11-22 10:19:12 +0000

    Method Over Loading Commited

---
+++ OldWayOfMethodOverloading.java
@@ -0,0 +1,12 @@
+package oldwayofmethodoverloading;
+
+public class OldWayOfMethodOverloading
+{
+    static int a = 35; static int b = 15; static double c = 50.50; static double d = 25.50;
+
+    public static void main( String[] args)
+    {
+        System.out.println(a+b);
+        System.out.println(c-d);
+    }
+}
